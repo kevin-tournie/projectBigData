06c7096d469d953900177a5246f591adeb2b7b92
Kévin Tournié <kevin.tournie37@gmail.com>
2022-12-12 04:15:07 +0100

    cleaner request

--- appbigdataproject/src/App.tsx
+++ appbigdataproject/src/App.tsx
@@ -1,11 +1,41 @@
+import {
+  Configuration,
+  CreateCompletionResponseChoicesInner,
+  OpenAIApi,
+} from "openai";
 import { useEffect, useState } from "react";
 import "./App.css";
-import { fetchingQuestionsAndAnswersFromOpenAI } from "./services/fetch";
+import { scriptFetchingAndInserting } from "./services/fetch";
 
 function App() {
-  const [QRArray, setQRArray] = useState([]);
+  const [QRArray, setQRArray] = useState<
+    CreateCompletionResponseChoicesInner[]
+  >([]);
   useEffect(() => {
-    fetchingQuestionsAndAnswersFromOpenAI(setQRArray);
+    // async function fetchingQuestionsAndAnswersFromOpenAI() {
+    //   const request: string =
+    //     "Give me a geography question in french in this template : 'Question /n Answer1 /n Answer2 /n Answer3 /n Answer4 /n CorrectAnswer'";
+
+    //   const configuration = new Configuration({
+    //     apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+    //   });
+    //   const openai = new OpenAIApi(configuration);
+    //   await openai
+    //     .createCompletion({
+    //       model: "text-davinci-003",
+    //       prompt: request,
+    //       temperature: 0,
+    //       max_tokens: 1000,
+    //     })
+    //     .then((data) => {
+    //       let splittedData = data.data.choices[0].text?.split("/n") || [];
+    //       let cleanQuestion = splittedData[0].slice(2);
+    //       console.log(splittedData, cleanQuestion);
+    //       setQRArray(data.data.choices);
+    //     });
+    // }
+    // fetchingQuestionsAndAnswersFromOpenAI();
+    scriptFetchingAndInserting();
   }, []);
   return <div className="App"></div>;
 }
--- appbigdataproject/src/services/fetch.ts
+++ appbigdataproject/src/services/fetch.ts
@@ -1,21 +1,57 @@
 import { Configuration, OpenAIApi } from "openai";
 
-export async function fetchingQuestionsAndAnswersFromOpenAI(setQRarray: any) {
-  const request: string =
-    "Je veux un ensemble de 10 questions aléatoires avec 4 choix possibles.";
+import { createClient } from "@supabase/supabase-js";
+
+export async function scriptFetchingAndInserting() {
+  const supabaseUrl = "https://cwdvvgqegcucriiaapuq.supabase.co";
+  const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+  const supabase = createClient(supabaseUrl, supabaseKey);
 
   const configuration = new Configuration({
     apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
-  await openai
-    .createCompletion({
-      model: "text-davinci-003",
-      prompt: request,
-      temperature: 0,
-      max_tokens: 1000,
-    })
-    .then((response) => {
-      setQRarray(response.data.choices);
-    });
+
+  const languages = ["French", "Spanish", "German", "English", "Italian"];
+  const categories = [
+    "culture générale",
+    "science",
+    "geographie",
+    "histoire",
+    "film",
+  ];
+
+  for (let category of categories) {
+    for (let i = 0; i < 10; i++) {
+      const request: string =
+        "Donne moi une question sur" +
+        category +
+        " avec ce format : 'Question / reponse1 / reponse2 / reponse3 / reponse4 / reponse correcte'";
+
+      await openai
+        .createCompletion({
+          model: "text-davinci-003",
+          prompt: request,
+          temperature: 1,
+          max_tokens: 1000,
+        })
+        .then((data) => {
+          let splittedData = data.data.choices[0].text?.split("/") || [];
+          return splittedData;
+        })
+        .then((splittedData) =>
+          supabase.from("QuestionsAndAnswers").insert({
+            category,
+            question: splittedData[0].slice(2),
+            answer1: splittedData[1],
+            answer2: splittedData[2],
+            answer3: splittedData[3],
+            answer4: splittedData[4],
+            correctAnswer: splittedData[5],
+            language: "French",
+          })
+        )
+        .catch((error) => console.log(error));
+    }
+  }
 }
