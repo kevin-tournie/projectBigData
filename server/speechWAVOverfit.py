bcb618e6602ab986e2f5c36c69be51fd2a8db1c1
Simon Huang <simon.huang@telecom-st-etienne.fr>
2023-01-28 14:04:01 +0100

    adding speechWAVOverfit.py to the right repository

---
+++ server/speechWAVOverfit.py
@@ -0,0 +1,30 @@
+import speech_recognition as sr
+
+#Input: Path of the wav file
+#Output: String of the transcripted text
+
+#Si l'un des résultats possibles est dans la liste des fréquence, on va renvoyer le résultat possible trouver même s'il n'a pas la fréquence la plus élevée
+def speechWAVOverfit(WAVFilePath):
+    r = sr.Recognizer()
+
+    audio_file_wav = sr.AudioFile(WAVFilePath)
+    with audio_file_wav as source:
+        audio = r.record(source)
+
+    possible_answers = ["yes", "no", "one", "two", "three", "four"]
+    #Reconnaissance de la parole
+    # Raises a ``speech_recognition.UnknownValueError`` exception if the speech is unintelligible. Raises a ``speech_recognition.RequestError`` exception if the speech recognition operation failed, if the key isn't valid, or if there is no internet connection.
+    try:
+        r.recognize_google(audio, language = "en-US",show_all=False)
+    except sr.UnknownValueError:
+        return sr.UnknownValueError
+    except sr.RequestError as e:
+        return sr.RequestError
+
+    result = r.recognize_google(audio, language = "en-US", show_all=True)
+    if result: 
+        for row in result["alternative"]:
+            if row["transcript"] in possible_answers:
+                return row["transcript"]
+        return result["alternative"][0]["transcript"]
+   
