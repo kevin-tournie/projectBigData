a78a9a094ddf2b05e641e9b6504a854b2a634f4b
kevin-tournie <kevin.tournie37@gmail.com>
2023-02-07 16:25:48 +0100

    add types on trivia fetch functions

--- appbigdataproject/src/services/trivia.ts
+++ appbigdataproject/src/services/trivia.ts
@@ -1,46 +1,78 @@
 import { maxQuestions } from "../const";
 import { shuffleArray } from "./utils";
 
-export interface IQuestion {
+type PreProcessedQuestion = {
   category: string;
   type: string;
   difficulty: string;
   question: string;
   correct_answer: string;
   incorrect_answers: string[];
-}
+};
+
+type TriviaAPIResponseStructure = {
+  response_code: number;
+  results: PreProcessedQuestion[];
+};
+
+type PostProcessedQuestion = PreProcessedQuestion & {
+  shuffledAnswers: string[];
+};
+
+type TriviaAPICategoryResponseStructure = {
+  trivia_categories: {
+    id: number;
+    name: string;
+  }[];
+};
 
 export const fetchQuestionsAnswers = async (
   selectedCategoryId: number,
   selectedDifficulty: string
-) => {
-  const result = await fetch(
-    `https://opentdb.com/api.php?amount=${maxQuestions}&category=${selectedCategoryId}&difficulty=${
-      selectedDifficulty.toLowerCase() === "super easy"
-        ? "easy"
-        : selectedDifficulty.toLowerCase()
-    }&type=${
-      selectedDifficulty.toLowerCase() === "super easy" ? "boolean" : "multiple"
-    }`
-  )
-    .then((data) => data.json())
-    .then((data) => {
-      data.results.forEach((question: any) => {
-        question.shuffledAnswers = shuffleArray([
-          question.correct_answer,
-          ...question.incorrect_answers,
-        ]);
+): Promise<PostProcessedQuestion[] | string> => {
+  try {
+    const questions = await fetch(
+      `https://opentdb.com/api.php?amount=${maxQuestions}&category=${selectedCategoryId}&difficulty=${
+        selectedDifficulty.toLowerCase() === "super easy"
+          ? "easy"
+          : selectedDifficulty.toLowerCase()
+      }&type=${
+        selectedDifficulty.toLowerCase() === "super easy"
+          ? "boolean"
+          : "multiple"
+      }`
+    );
+
+    const questionsJSON: TriviaAPIResponseStructure = await questions.json();
+
+    const processedQuestions: PostProcessedQuestion[] =
+      questionsJSON.results.map((question) => {
+        return {
+          ...question,
+          shuffledAnswers: shuffleArray([
+            question.correct_answer,
+            ...question.incorrect_answers,
+          ]),
+        };
       });
-      return data;
-    });
-  return result;
+
+    return processedQuestions;
+  } catch (error) {
+    console.log(error);
+    return "Can't fetch questions:" + error;
+  }
 };
 
-export const fetchAllCategories = async () => {
-  const result = await fetch("https://opentdb.com/api_category.php")
-    .then((data) => data.json())
-    .then((data) => {
-      return data;
-    });
-  return result;
+export const fetchAllCategories = async (): Promise<
+  TriviaAPICategoryResponseStructure | string
+> => {
+  try {
+    const categories = await fetch("https://opentdb.com/api_category.php");
+    const categoriesJSON: TriviaAPICategoryResponseStructure =
+      await categories.json();
+    return categoriesJSON;
+  } catch (error) {
+    console.log(error);
+    return "Can't fetch categories:" + error;
+  }
 };
