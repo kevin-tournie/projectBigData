974de9db5e1baee8d75af6bd71c578a98345634a
MEZIDI Abdel-Rahim <ar.mezidi@gmail.com>
2023-02-08 14:23:00 +0100

    Big commit : added lstm, refactored dir structure

---
+++ Models/src/googleApi/speechFileMP3ToTextFunction.py
@@ -0,0 +1,44 @@
+# #Commande: python .\speechFileMP3ToTextFunction.py
+# #Input: Path of the mp3 file
+# #Output: String of the transcripted text
+
+
+import os
+from pydub import AudioSegment
+import speech_recognition as sr
+
+def speechFileMP3ToTextFunction(MP3FilePath):
+    # Extract the filename without the extension to use as the WAV file name
+    # file_name = os.path.splitext(os.path.basename(MP3FilePath))[0]
+    file_name = os.path.splitext(MP3FilePath)[0]
+
+    # Initialize a Recognizer object
+    r = sr.Recognizer()
+
+    # Read the audio file in MP3 format
+    sound = AudioSegment.from_mp3(MP3FilePath)
+
+    
+
+    # Convert the MP3 file to WAV format and save it to the "target" directory
+    # sound.export(file_name + "test" + ".wav", format="wav")
+    sound.export(file_name + ".wav", format="wav")
+    # sound.export("./en_test_subset/target/" + file_name + ".wav", format="wav")
+
+
+
+    # Open the WAV file
+    # audio_file_wav = sr.AudioFile("./target/" + file_name + ".wav")
+    audio_file_wav = sr.AudioFile(file_name + ".wav")
+    # audio_file_wav = sr.AudioFile("./en_test_subset/target/" + file_name + ".wav")
+    print("./en_test_subset/target/" + file_name + ".wav")
+    with audio_file_wav as source:
+        audio = r.record(source)
+
+    # Perform speech recognition on the recorded audio
+    try:
+        return r.recognize_google(audio, language = "en-US",show_all=False)
+    except sr.UnknownValueError:
+        return sr.UnknownValueError
+    except sr.RequestError as e:
+        return sr.RequestError
---
+++ Models/src/utils/creating_test_files_subset.py
@@ -0,0 +1,35 @@
+#Creating en_test_full_subset (Yes/No 1-4)
+import pandas as pd
+import os
+from shutil import copyfile
+
+
+# Exemple de path1
+# path1 = "C:/Users/Simon/OneDrive/Documents/FISE3/Semestre 9/Spe-Info2-Big Data/Big Data Project/en/clips/"
+# C'est le path où se trouve l'ensemble des fichiers audio
+
+def creating_test_files_subset(path1):
+    # Charger le fichier filtered_test.tsv en utilisant pandas
+    filtered_test_df = pd.read_csv('./tsv_files/filtered_test.tsv', sep='\t')
+    # Définir le path3
+    path3 = './en_test_full_subset/'
+
+    # Vérifier si le path3 existe, sinon le créer
+    if not os.path.exists(path3):
+        os.makedirs(path3)
+
+    # Pour chaque ligne du fichier filtered_test.tsv
+    for index, row in filtered_test_df.iterrows():
+        # Récupérer le nom du fichier audio
+        audio_file_name = row['path']
+        # Construire le chemin complet du fichier audio dans path1
+        source_path = os.path.join(path1, audio_file_name)
+        # Construire le chemin complet où le fichier audio sera copié dans path3
+        destination_path = os.path.join(path3, audio_file_name)
+        # Vérifier si le fichier audio existe dans path1
+        if os.path.exists(source_path):
+            # Copier le fichier audio de path1 à path3
+            copyfile(source_path, destination_path)
+        else:
+            print(f"Le fichier {audio_file_name} n'existe pas dans {path1}")
+    return filtered_test_df
