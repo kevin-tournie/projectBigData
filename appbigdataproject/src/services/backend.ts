b2a54c0617d1ebdc3a961b92ee2664e96582aed7
MEZIDI Abdel-Rahim <ar.mezidi@gmail.com>
2023-02-09 18:46:37 +0100

    added routes for yes_no

--- appbigdataproject/src/services/backend.ts
+++ appbigdataproject/src/services/backend.ts
@@ -1,3 +1,23 @@
+export const processAudio_yes_no = async (blob: Blob) => {
+  const formData = new FormData();
+  formData.append("file",blob,"test.wav")
+  const data = await fetch(import.meta.env.VITE_BACKEND_BASE_URL + "/api/processAudio_yes_no", {
+    method: "POST",
+    body: formData,
+    mode:"cors",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    /*headers: {
+      "Content-Type": "multipart/form-data"
+    },*/
+  })
+
+  const result = await data.text()
+
+  return result;
+}
+
 export const processAudio = async (blob: Blob) => {
   const formData = new FormData();
   formData.append("file",blob,"test.wav")
